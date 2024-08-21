7c3b9d6138fca5d113519a164339256078190e6b
abdulrojak12345678 <rojaka140507@gmail.com>
2024-08-21 08:56:49 +0700

    update

--- main.js
+++ main.js
@@ -235,3 +235,64 @@ export async function ambilproduk(docId) {
   return await docSnap.data();
 }
 
+
+export async function ambilDaftarmapel() {
+  const refDokumen = collection(db, "jadwal-mapel");
+  const kueri = query(refDokumen, orderBy("mapel"));
+  const cuplikanKueri = await getDocs(kueri);
+
+  let hasil = [];
+  cuplikanKueri.forEach((dok) => {
+    hasil.push({
+      id: dok.id,
+      hari: dok.data().hari,
+      waktu: dok.data().waktu,
+      kelas: dok.data().kelas,
+      mapel: dok.data().mapel,
+      gurumapel:dok.data().gurumapel
+    });
+  });
+
+
+  return hasil
+}
+
+//export function formatAngka(x) {
+//return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g,".");
+  
+  export async function tambahjadwalmapel(hari, waktu, kelas, mapel, gurumapel) {
+  try {
+    const dokRef = await addDoc(collection(db, 'jadwal-mapel'), {
+      hari: hari,
+      waktu: waktu,
+      kelas: kelas,
+      mapel: mapel,
+     gurumapel:gurumapel
+    });
+    console.log('berhasil menembah jadwal ' + dokRef.id);
+  } catch (e) {
+    console.log('gagal menambah jadwal' + e);
+  }
+}
+
+//fungsi untuk hapus data
+export async function hapusmapel(docId) {
+  await deleteDoc(doc(db, "jadwal-mapel", docId));
+}
+//fungsi untuk ubah data
+export async function ubahdaftarmapel(docId, hari, waktu, kelas, mapel, gurumapel) {
+  await updateDoc(doc(db, "jadwal-mapel", docId), {
+    hari: hari,
+    waktu: waktu,
+    kelas: kelas,
+    mapel: mapel,
+    gurumapel: gurumapel
+  });
+}
+//fungsi untuk ambil data dan untuk diubah
+export async function ambildaftarmapel(docId) {
+  const docRef = await doc(db, "jadwal-mapel", docId);
+  const docSnap = await getDoc(docRef);
+
+  return await docSnap.data();
+}
