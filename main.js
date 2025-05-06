35a8320232411b2a36ba814828612755eb6b768f
abdulrojak12345678 <rojaka140507@gmail.com>
2025-05-06 21:25:35 +0700

    update

--- main.js
+++ main.js
@@ -405,3 +405,56 @@ export async function ubahStatusTugas(id, statusBaru) {
       status: statusBaru,
   });
 }
+// export pkl
+export async function ambilDaftarBarang() {
+  const refDokumen = collection(db, "pkl");
+  const kueri = query(refDokumen, orderBy("namaBarang"));
+  const cuplikanKueri = await getDocs(kueri);
+
+  let hasil = [];
+  cuplikanKueri.forEach((dok) => {
+    hasil.push({
+      id: dok.id,
+      tanggalMasuk: dok.data(). tanggalMasuk,
+      namaBarang: dok.data(). namaBarang,
+      jumlahBarang: dok.data(). jumlahBarang
+    });
+  });
+
+
+  return hasil
+}
+
+export async function tambahBarang(tanggalMasuk, namaBarang, jumlahBarang,) {
+  try {
+    const dokRef = await addDoc(collection(db, 'pkl'), {
+      tanggalMasuk: tanggalMasuk,
+      namaBarang: namaBarang,
+      jumlahBarang: jumlahBarang
+    });
+    console.log('berhasil menembah Barang ' + dokRef.id);
+  } catch (e) {
+    console.log('gagal menambah Barang' + e);
+  }
+}
+
+
+//fungsi untuk hapus data
+export async function hapusBarang(docId) {
+  await deleteDoc(doc(db, "pkl", docId));
+}
+//fungsi untuk ubah data
+export async function ubahBarang(docId,tanggalMasuk, namaBarang, jumlahBarang ) {
+  await updateDoc(doc(db, "pkl", docId), {
+    tanggalMasuk: tanggalMasuk,
+    namaBarang: namaBarang,
+    jumlahBarang: jumlahBarang
+  });
+}
+//fungsi untuk ambil data dan untuk diubah
+export async function ambilBarang(docId) {
+  const docRef = await doc(db, "pkl", docId);
+  const docSnap = await getDoc(docRef);
+
+  return await docSnap.data();
+}
